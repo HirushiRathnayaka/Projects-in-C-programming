f0243a8ac2b5f2e8b7e51bfab8aea15ef531739d
Hirushi Rathnayaka <hirushishashikala2000@gmail.com>
2025-09-19 02:40:00 +0530

    Task_02

---
+++ Task_02/Task_02/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a[10];
+    int max=0,min=0,sum=0;
+    float avg=0;
+
+    for(int i=0;i<10;i++)
+    {
+        printf("Enter a number ");
+        scanf("%d",&a[i]);
+
+
+        if(max<a[i])
+        {
+            max=a[i];
+        }
+
+        else if(min>a[i])
+        {
+            min=a[i];
+        }
+
+        sum+=a[i];
+
+    }
+
+    avg=(float) sum/10;
+    printf("Maximum value is %d \nMinimum value is %d \nAverage value is %.2f\n",max,min,avg);
+
+    for (int i=10;i>=0;i--)
+    {
+        printf("%d\n",a[i]);
+    }
+
+    return 0;
+}
